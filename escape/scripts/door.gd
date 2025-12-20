5f3a28ffa983b7bdc3e0f56cc60be27ba0f9e863
Thibault Lenclos <tlenclos@users.noreply.github.com>
2025-12-20 22:12:14 +0100

    feat: debug animation door on web

--- escape/scripts/door.gd
+++ escape/scripts/door.gd
@@ -3,4 +3,5 @@ extends Node3D
 @onready var animation_player: AnimationPlayer = $"../../../AnimationPlayer"
 
 func open() -> void:
+	print(str(animation_player.get_animation_list()))
 	animation_player.play('open')
